6f570934f3a5abba6f7af4b27f707c6dc19387d8
Sandipan Seth <135996651+Sandipan-seth@users.noreply.github.com>
2023-12-08 12:46:42 +0530

    Add smooth scroll
    Smooth scrolling using locomotive js codepen

--- rejouice/index.js
+++ rejouice/index.js
@@ -1,38 +1,45 @@
 function smoothScroll() {
     gsap.registerPlugin(ScrollTrigger);
 
+// Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
+
+const locoScroll = new LocomotiveScroll({
+  el: document.querySelector("#main"),
+  smooth: true
+});
+// each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+locoScroll.on("scroll", ScrollTrigger.update);
+
+// tell ScrollTrigger to use these proxy methods for the "#main" element since Locomotive Scroll is hijacking things
+ScrollTrigger.scrollerProxy("#main", {
+  scrollTop(value) {
+    return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+  }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+  getBoundingClientRect() {
+    return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
+  },
+  // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+  pinType: document.querySelector("#main").style.transform ? "transform" : "fixed"
+});
+
+
+
+
+
+
+
+
+// each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
+ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+// after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+ScrollTrigger.refresh();
+
     
-
-    const locoScroll = new LocomotiveScroll({
-        el: document.querySelector("#main"),
-        smooth: true
-    });
-
-    locoScroll.on("scroll", ScrollTrigger.update);
-
-    ScrollTrigger.scrollerProxy(".smooth-scroll", {
-        scrollTop(value) {
-            return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
-        },
-        getBoundingClientRect() {
-            return { top: 0, left: 0, width: window.innerWidth, height: window.innerHeight };
-        },
-        pinType: document.querySelector(".smooth-scroll").style.transform ? "transform" : "fixed"
-    });
-
-
-    
-
-
-
-    ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
-
-
-    ScrollTrigger.refresh();
 
 }
 
-// smoothScroll()
+smoothScroll()
 
 
 function cursorEvent() {
@@ -49,7 +56,6 @@ function cursorEvent() {
     page1.addEventListener("mouseenter", () => {
         gsap.to(cursor, {
             scale: 1,
-            // delay:10
             opacity: 1
         })
     })
@@ -103,12 +109,14 @@ function loading(){
     })
     
     tl.from("#page1-content h1 span",{
+        // scrub:2,
         y:100,
         opacity:0,
         stagger:0.1
     })
     
     tl.from("#page1-content nav h3",{
+        // scrub:2,
         delay:-1,
         y:30,
         opacity:0,
@@ -124,13 +132,14 @@ function page2Animation(){
         opacity:0,
         y:50,
         duration:1,
-        stagger:0.3,
+        stagger:0.1,
         scrollTrigger:{
             trigger:"#page2",
             start:"top 50%",
             end:"top 0",
-            scroller:"body", 
+            scroller:"#main", 
             // markers: true,
+            // pin:true,
             scrub:3
         },
     })
@@ -146,10 +155,11 @@ function page3Animation(){
         duration:1,
         stagger:0.3,
         scrollTrigger:{
+            // pin:true,
             trigger:"#page3",
-            start:"top 50%",
-            end:"top 0",
-            scroller:"body", 
+            start:"top 70%",
+            end:"top 30%",
+            scroller:"#main", 
             // markers: true,
             scrub:3
         },
